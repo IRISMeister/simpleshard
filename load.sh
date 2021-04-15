45aeacf1814b3e8c0cd3ce60a021d3ae95ec1c08
IRISMeister <12710874+IRISMeister@users.noreply.github.com>
2021-04-15 18:28:28 +0900

    add TuneTable

--- load.sh
+++ load.sh
@@ -6,3 +6,4 @@ docker-compose exec loader /app/loader/shell/tx_table_main.sh
 docker-compose exec loader /app/loader/shell/tx_table_sub1.sh
 docker-compose exec loader /app/loader/shell/tx_table_sub2.sh
 
+docker-compose exec data01 iris session iris -U IRISDM tunetables
