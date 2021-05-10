f34d873862554325e03ecae971d1b11bee0d21d6
IRISMeister <12710874+IRISMeister@users.noreply.github.com>
2021-05-10 08:29:01 +0000

    fix

--- k8s/start.sh
+++ k8s/start.sh
@@ -8,14 +8,20 @@ microk8s kubectl apply -f simple.yml
 #echo "microk8s kubectl describe pod -l app=iris"
 #echo "microk8s kubectl logs data-0"
 
+echo "waiting until all pods are running"
+# may need a more robust way
+count=0
+while [ "$count" -lt "3" ]
+do
+sleep 1
+# note Pod 'Running' is not equal to IRIS 'Running'
+count=$(microk8s kubectl get pod -l app=iris --field-selector=status.phase=Running | wc | awk '{print $1}')
+done
+
 echo "creating shard cluster"
-# may need a pause here
-microk8s kubectl wait pod/data-0 --for condition=Ready --timeout=30s
-microk8s kubectl wait pod/data-1 --for condition=Ready --timeout=30s
-microk8s kubectl wait pod/data-2 --for condition=Ready --timeout=30s
-sleep 5
 ./create-shard-cluster.sh
 
+# start java based csv loader
 microk8s kubectl apply -f loader.yml
 sleep 5
 microk8s kubectl exec -it loader -- /app/loader/shell/sales.sh
--- loader/shell/envs.sh
+++ loader/shell/envs.sh
@@ -1,6 +1,3 @@
-instanceName=iris
-namespace=bigdata
-user=_SYSTEM
-pass=sys
-IRISJAR=/app/
-ISCCLASSLIBS=$IRISJAR/intersystems-utils-3.2.0.jar:$IRISJAR/intersystems-jdbc-3.2.0.jar:$IRISJAR/intersystems-xep-3.2.0.jar
+#!/bin/sh
+export IRISJAR=/app/
+export ISCCLASSLIBS=$IRISJAR/intersystems-utils-3.2.0.jar:$IRISJAR/intersystems-jdbc-3.2.0.jar:$IRISJAR/intersystems-xep-3.2.0.jar
