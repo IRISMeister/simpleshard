1538e44c198dee707b51226a3db3569051741458
IRISMeister <12710874+IRISMeister@users.noreply.github.com>
2021-05-12 03:41:38 +0000

    add a very simple java code

---
+++ loader/build/src/main/java/build_and_run.sh
@@ -0,0 +1,3 @@
+
+javac com/example/App.java
+java -cp /app/intersystems-jdbc-3.2.0.jar:. com/example/App
---
+++ loader/build/src/main/java/com/example/App.java
@@ -0,0 +1,72 @@
+package com.example;
+import java.sql.*;
+import java.util.Properties;
+import java.util.Random;
+
+public class App 
+{
+    protected   static  String              namespace = "IRISDM";
+    protected   static  String              host = "data-0.iris";
+    protected   static  int                 port = 1972; 
+    protected   static  String              url = "jdbc:IRIS://"+host+":" + port + "/"+namespace;
+    protected   static  String              username = "SuperUser";
+    protected   static  String              password = "SYS";
+    
+    public static void main( String[] args )
+    {
+        Connection dbconnection = null;
+        PreparedStatement pstmt = null;
+        int itemcnt;
+        int cnt=0;
+
+        System.out.println( "Connecting to IRIS server on "+host );
+
+        try {
+            Class.forName ("com.intersystems.jdbc.IRISDriver");
+            Properties p = new Properties();
+            p.setProperty("SharedMemory", "false");
+            p.setProperty("user", username);
+            p.setProperty("password", password);
+            dbconnection = DriverManager.getConnection(url,p);
+            dbconnection.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
+            dbconnection.setAutoCommit(true);
+            Random rand = new Random();
+
+            //TempoID,ItemID,OrderDate,price
+
+            String isql = "insert into sales (TempoID,ItemID,OrderDate,price) values ( ?, ?, ?, ?)";
+            pstmt = dbconnection.prepareStatement(isql);
+            for (int i=1; i<=100; i++) {
+                pstmt.setString(1, String.valueOf(i));
+                itemcnt=10000;
+                for (int j=1; j<=itemcnt; j++) {
+                    pstmt.setString(2, String.valueOf(j));
+                    pstmt.setString(3, "2021-01-01");
+                    pstmt.setInt(4,(rand.nextInt(100)+1)*10 );
+                    pstmt.execute();
+                    cnt++;
+                }
+            }
+            pstmt.close();
+            System.out.println(cnt+" records inserted");
+            /*
+    		pstmt = dbconnection.prepareStatement("SELECT 1 col1");
+            ResultSet rs = pstmt.executeQuery();
+            while(rs.next()){
+                System.out.println(rs.getString("col1"));
+            }
+			pstmt.close();
+            */
+
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            try {
+                dbconnection.close();
+            } catch (Exception ex) {
+            }
+        }    
+
+    }
+}
